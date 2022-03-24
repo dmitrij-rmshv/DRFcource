399ce8518a67e85ee27dd447ce2978516aacec84
dmitrij-rmshv <gdems@fastmail.com>
2022-03-24 23:18:08 +0300

    lsn03almost

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from main.views import TodoUserCustomViewSet
 from todoapp.views import TodoProjectModelViewSet, TodoNoteModelViewSet
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
 router.register('users', TodoUserCustomViewSet)
@@ -28,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
