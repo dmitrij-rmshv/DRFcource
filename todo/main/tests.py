dc5561fa107cf0f934c2177b826e846f690fcc2b
dmitrij-rmshv <gdems@fastmail.com>
2022-04-04 22:57:31 +0300

    almost3

--- todo/main/tests.py
+++ todo/main/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+# from mixer.backend.django import mixer
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .views import TodoUserCustomViewSet
+from .models import TodoUser
+
+
+class TestTodoUserCustomViewSet(TestCase):
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = TodoUserCustomViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    
+    # def test_create_admin(self):
+    #     factory = APIRequestFactory()
+    #     request = factory.post('/api/users/', {'username': 'Пушкин'}, format='json')
+    #     admin = TodoUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+    #     force_authenticate(request, admin)
+    #     view = TodoUserCustomViewSet.as_view({'post': 'create'})
+    #     response = view(request)
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # def test_get_detail(self):
+    #     author = TodoUser.objects.create(username='Пушкин')
+    #     client = APIClient()
+    #     response = client.get(f'/api/users/{author.uid}/')
+    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_detail(self):
+        author = TodoUser.objects.create(username='Пупкин')
+        client = APIClient()
+        response = client.get(f'/api/users/{author.uid}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_edit_admin(self):
+        author = TodoUser.objects.create(username='Пупкин')
+        client = APIClient()
+        admin = TodoUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        client.login(username='admin', password='admin123456')
+        response = client.put(f'/api/users/{author.uid}/', {'username':'Залупкин'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_author = TodoUser.objects.get(uid=author.uid)
+        self.assertEqual(new_author.username, 'Залупкин')
+        client.logout()
--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+# from mixer.backend.django import mixer
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .views import TodoProjectModelViewSet, TodoNoteModelViewSet
+from .models import Project, ToDo
+from main.models import TodoUser
+
+
+class TestTodoProjectModelViewSet(APITestCase):
+    def test_get_list(self):
+        response = self.client.get('/api/projects/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_edit_admin(self):
+        author = TodoUser.objects.create(username='Пупкин')
+        project = Project.objects.create(title='Geekshop')
+        project.developers.add(author)
+        admin = TodoUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        self.client.login(username='admin', password='admin123456')
+        response = self.client.put(f'/api/projects/{project.id}/', {'title': 'Сетевой чат'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        project = Project.objects.get(id=project.id)
+        self.assertEqual(project.title,'Сетевой чат')
