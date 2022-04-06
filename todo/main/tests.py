19e6fb32770ef79a6793287ad60d0c39e5959f49
dmitrij-rmshv <gdems@fastmail.com>
2022-04-06 16:51:39 +0300

    lsn08

--- todo/main/tests.py
+++ todo/main/tests.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
-# from mixer.backend.django import mixer
-from django.contrib.auth.models import User
+from rest_framework.test import APIRequestFactory, APIClient, APITestCase
 
 from .views import TodoUserCustomViewSet
 from .models import TodoUser
@@ -17,21 +15,6 @@ class TestTodoUserCustomViewSet(TestCase):
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
-    # def test_create_admin(self):
-    #     factory = APIRequestFactory()
-    #     request = factory.post('/api/users/', {'username': 'Пушкин'}, format='json')
-    #     admin = TodoUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
-    #     force_authenticate(request, admin)
-    #     view = TodoUserCustomViewSet.as_view({'post': 'create'})
-    #     response = view(request)
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    # def test_get_detail(self):
-    #     author = TodoUser.objects.create(username='Пушкин')
-    #     client = APIClient()
-    #     response = client.get(f'/api/users/{author.uid}/')
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
     def test_get_detail(self):
         author = TodoUser.objects.create(username='Пупкин')
         client = APIClient()
@@ -48,3 +31,15 @@ class TestTodoUserCustomViewSet(TestCase):
         new_author = TodoUser.objects.get(uid=author.uid)
         self.assertEqual(new_author.username, 'Залупкин')
         client.logout()
+
+
+class ApiTestTodoUserCustomViewSet(APITestCase):
+
+    def test_edit_admin(self):
+        author = TodoUser.objects.create(username='Пупкин')
+        admin = TodoUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        self.client.login(username='admin', password='admin123456')
+        response = self.client.put(f'/api/users/{author.uid}/', {'username':'Залупкин'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_author = TodoUser.objects.get(uid=author.uid)
+        self.assertEqual(new_author.username, 'Залупкин')
--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -1,26 +1,29 @@
-from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
-# from mixer.backend.django import mixer
-from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from mixer.backend.django import mixer
 
-from .views import TodoProjectModelViewSet, TodoNoteModelViewSet
-from .models import Project, ToDo
+from .models import ToDo
 from main.models import TodoUser
 
 
 class TestTodoProjectModelViewSet(APITestCase):
+
     def test_get_list(self):
         response = self.client.get('/api/projects/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_edit_admin(self):
-        author = TodoUser.objects.create(username='Пупкин')
-        project = Project.objects.create(title='Geekshop')
-        project.developers.add(author)
+
+class TestTodoNoteModelViewSet(APITestCase):
+
+    def test_get_list(self):
+        response = self.client.get('/api/notes/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_edit_mixer(self):
+        note = mixer.blend(ToDo)
         admin = TodoUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
         self.client.login(username='admin', password='admin123456')
-        response = self.client.put(f'/api/projects/{project.id}/', {'title': 'Сетевой чат'})
+        response = self.client.put(f'/api/notes/{note.id}/', {'text': 'Убрать авторизацию!'})
+        # self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        project = Project.objects.get(id=project.id)
-        self.assertEqual(project.title,'Сетевой чат')
+
